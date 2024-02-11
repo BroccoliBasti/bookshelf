46bed0bcfad5736e81b7c676c6162b6fa3d262d9
Sebastian Renkonen <100607489+BroccoliBasti@users.noreply.github.com>
2024-02-11 13:36:02 +0200

    implement custom font

--- Bookshelf/pages/index.vue
+++ Bookshelf/pages/index.vue
@@ -1,8 +1,9 @@
 <template>
-    <h1 class="font-bold text-4xl text-center my-6">Hello Pörld!</h1>
+    <h1 class="font-bold text-4xl text-indigo-950 text-center my-6">My bookshelf</h1>
 
     <div class="grid sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-6 m-6">
-        <div v-for="book in books" :key="book.id" :book="book" class="p-12 font-bold bg-sky-100">
+        <div v-for="book in books" :key="book.id" :book="book"
+            class=" p-4 aspect-[19/24] font-bold font-morris text-xl ring-2 ring-indigo-100 rounded-sm hover:bg-indigo-200/50 cursor-pointer text-center text-indigo-950">
             {{ book.title }}
         </div>
     </div>
