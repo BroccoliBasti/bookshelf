d76c03223f24b3acec155aa1e1f3703249a57468
Sebastian Renkonen <100607489+BroccoliBasti@users.noreply.github.com>
2024-02-16 19:39:08 +0200

    fix titlebar bug

--- Bookshelf/layouts/default.vue
+++ Bookshelf/layouts/default.vue
@@ -1,14 +1,28 @@
 <template>
-    <NuxtPage />
+    <div data-tauri-drag-region class="titlebar flex justify-end rounded-t-md bg-indigo-500/10">
+        <div class="titlebar-button p-2 hover:bg-indigo-200 active:ring-2 active:ring-amber-50" id="titlebar-minimize"
+            @click="appWindow.minimize()">
+            <img src="https://api.iconify.design/mdi:window-minimize.svg" alt="minimize" />
+        </div>
+        <div class="titlebar-button p-2 hover:bg-indigo-200 active:ring-2 active:ring-amber-50" id="titlebar-maximize"
+            @click="appWindow.toggleMaximize()">
+            <img src="https://api.iconify.design/mdi:window-maximize.svg" alt="maximize" />
+        </div>
+        <div class="titlebar-button p-2 hover:bg-indigo-200 active:ring-2 active:ring-amber-50" id="titlebar-close"
+            @click="appWindow.close()">
+            <img src="https://api.iconify.design/mdi:close.svg" alt="close" />
+        </div>
+    </div>
+    <NuxtPage class="overflow-auto" />
 </template>
 
 <style>
-body {
-    @apply bg-amber-50
-}
-
 * {
-    @apply transform
+    scrollbar-width: none;
+
+    &::-webkit-scrollbar {
+        display: none;
+    }
 }
 
 *::selection {
@@ -19,7 +33,11 @@ body {
     @apply bg-indigo-100 text-indigo-700
 }
 
-/* *::-webkit-scrollbar-button {
-    @apply bg-indigo-700
-} */
+html, body {
+    @apply bg-amber-50 text-indigo-950 rounded-t-md
+}
 </style>
+
+<script setup>
+import { appWindow } from '@tauri-apps/api/window';
+</script>
